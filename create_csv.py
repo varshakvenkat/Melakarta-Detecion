5de2d16d9c806e7631620b83bddccb48ccee9120
Varsha Venkata Krishnan <47625221+varshakvenkat@users.noreply.github.com>
2021-11-24 20:01:36 -0500

    Create both input and target csv files

--- create_csv.py
+++ create_csv.py
@@ -9,10 +9,14 @@ import sys
 folder_name = sys.argv[1]
 df = pd.DataFrame()
 mfccs = []
+classification = []
 for file_name in os.listdir(folder_name):
     print(file_name)
+    classification.append(file_name.split('_')[0])
     X, sample_rate = librosa.load(folder_name+file_name, res_type='kaiser_fast') 
     _mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0) 
     mfccs.append(_mfccs)
 
 pd.DataFrame(mfccs).to_csv(sys.argv[2])
+target = pd.get_dummies(pd.DataFrame(classification))
+target.to_csv(sys.argv[3])
